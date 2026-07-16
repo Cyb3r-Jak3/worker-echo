7acb639a7fe4fc33ef5faf42d3227f6ca90a4e5d
Cyb3r-Jak3 <git@cyberjake.xyz>
2026-07-15 22:17:09 -0400

    Update to pnpm 11
    Signed-off-by: Cyb3r-Jak3 <git@cyberjake.xyz>

--- test/index.spec.ts
+++ test/index.spec.ts
@@ -1,10 +1,6 @@
 // test/index.spec.ts
-import { env, createExecutionContext, waitOnExecutionContext, SELF } from 'cloudflare:test';
+import { SELF } from 'cloudflare:test';
 import { describe, it, expect } from 'vitest';
-
-// For now, you'll need to do something like this to get a correctly-typed
-// `Request` to pass to `worker.fetch()`.
-const IncomingRequest = Request<unknown, IncomingRequestCfProperties>;
 
 describe('/', () => {
 	it('200 /', async () => {
